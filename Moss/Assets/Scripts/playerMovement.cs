33fe88111fc7658327d83718dfdf126406736eab
Jacob Morgan <jake2002.jm@gmail.com>
2020-02-10 20:04:19 +0000

    Combat Animation
    easy

--- Moss/Assets/Scripts/playerMovement.cs
+++ Moss/Assets/Scripts/playerMovement.cs
@@ -3,15 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState{ // This is to tell what state the player is in
+    walk,
+    attack,
+    interact
+}
+
 public class playerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    public PlayerState currentState; // Current state of the player
     public float speed; // Speed of the player
     private Rigidbody2D playerRigidbody; // Gives the player a rigidbody type meaning that they will have physics
-    private UnityEngine.Vector3 playerChange;
-    private Animator animator;
+    private UnityEngine.Vector3 playerChange; // Change of x and y of the player
+    private Animator animator; 
     void Start()
     {
+        currentState = PlayerState.walk; // sets by default the player to walk
         animator = GetComponent<Animator>();
         playerRigidbody = GetComponent<Rigidbody2D>(); // Imports the component Rigidbody2D
     }
@@ -20,11 +28,33 @@ public class playerMovement : MonoBehaviour
     void Update()
     {
         playerChange = UnityEngine.Vector3.zero; // This Resets the player every frame
-        playerChange.x = Input.GetAxisRaw("Horizontal");
-        playerChange.y = Input.GetAxisRaw("Vertical");
+        playerChange.x = Input.GetAxisRaw("Horizontal"); //Gets the horizontal direction of the player
+        playerChange.y = Input.GetAxisRaw("Vertical"); // gets the vertical direction of the player
+        if(Input.GetButtonDown("attack")&& currentState != PlayerState.attack) // asks if the player is already attacking and if the attacking button is being pressed
+        {
+            StartCoroutine(AttackCo()); // Starts co routine below
+        }
+        else if(currentState == PlayerState.walk) //Else if stops the player so they cant run and attack, asks if the plakyer is walking
+        {
+            UpdateAnimationAndMove(); // Calls this function
+        }
+    }
+
+    private IEnumerator AttackCo() // Starts co routine
+    {
+        animator.SetBool("attacking",true);// Sets attacking to true in the animator
+        currentState = PlayerState.attack; // Changes the players state to attacking
+        yield return null ; // Stops for 1 second
+        animator.SetBool("attacking",false); // Sets attacking to false
+        yield return new WaitForSeconds(.3f); // stops for a third of a second
+        currentState = PlayerState.walk; //sets player state to walk
+    }
+
+    void UpdateAnimationAndMove()
+    {
         if(playerChange != UnityEngine.Vector3.zero) // Checks that something is happening
         {
-            movePlayer();
+            movePlayer(); // moves player
             animator.SetFloat("moveX",playerChange.x);
             animator.SetFloat("moveY",playerChange.y);
             animator.SetBool("moving",true);
